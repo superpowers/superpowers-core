8ac7d57c3308bc93eb51f9ae99f6c73ef13d0b30
Bilou84 <nicolas.gauthier84@gmail.com>
2015-04-07 17:33:30 +0200

    SupAPI: Fix registerPlugin parameter type

--- SupAPI/SupAPI.d.ts
+++ SupAPI/SupAPI.d.ts
@@ -1,5 +1,5 @@
 declare module SupAPI {
   var contexts: any;
   function registerPlugin(contextName: string, pluginName: string,
-    plugin: {code: string, defs: string, exposeActorComponent?: {propertyName: string; className: string}});
+    plugin: {code: string; defs: string; exposeActorComponent?: {propertyName: string; className: string};});
 }
