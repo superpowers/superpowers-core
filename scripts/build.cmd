9c04939298bccae1688a1d5b110ba10871cfa3c8
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-01 07:33:22 +0200

    scripts: Run gulp in server/, reformat Windows scripts
    --HG--
    branch : server-typescript

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,34 +1,13 @@
 pushd %~dp0
-
-cd ..
-call npm install
-
-cd SupCore
-call gulp
-
-cd ../SupClient
-call gulp
-
-cd ../SupAPI
-call gulp
-
-cd ../system/SupEngine
-call npm install
-call gulp
-
-cd ../SupRuntime
-call gulp
-
-cd ../player
-call gulp
-
-cd ../../client
-call gulp
-
-cd ../launcher
-call gulp
-
-cd ../scripts
-call buildPlugins.cmd
-
+cd .. & call npm install
+cd SupCore & call gulp
+cd ../SupClient & call gulp
+cd ../SupAPI & call gulp
+cd ../server & call gulp
+cd ../system/SupEngine & call npm install & call gulp
+cd ../SupRuntime & call gulp
+cd ../player & call gulp
+cd ../../client & call gulp
+cd ../launcher & call gulp
+cd ../scripts & call buildPlugins.cmd
 popd
