6a365a520363830b926686eecca1441d2dec75d6
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-04 11:45:31 +0200

    SupCore: Improve typing of TreeById.walk and Entries.walk

--- SupCore/SupCore.d.ts
+++ SupCore/SupCore.d.ts
@@ -84,6 +84,7 @@ declare module SupCore {
       dependenciesByAssetId: any;
 
       constructor(pub: EntryNode[], nextId?: number);
+      walk(callback: (node: EntryNode, parentNode?: EntryNode) => any): void;
       add(node: EntryNode, parentId: string, index: number, callback: (err: string, index?: number) => any): void;
       client_add(node: EntryNode, parentId: string, index: number): void;
       move(id: string, parentId: string, index: number, callback: (err: string, index?: number) => any): void;
@@ -203,7 +204,7 @@ declare module SupCore {
         parentNodesById: { [key: string]: any };
 
         constructor(pub: TreeNode[], schema: any, nextId?: number);
-        walk(callback: (node: TreeNode, parentNode?: any) => any): void;
+        walk(callback: (node: TreeNode, parentNode?: TreeNode) => any): void;
         walkNode(node: TreeNode, parentNode: TreeNode, callback: (node: TreeNode, parentNode?: TreeNode) => any): void;
         getPathFromId(id: string): string;
         add(node: TreeNode, parentId: string, index: number, callback: (err: string, index?: number) => any): void;
