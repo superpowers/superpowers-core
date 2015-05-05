7131cd89c158fe9d0f170b0f5fd40dc98fb26039
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-05 11:10:40 +0200

    SupClient: Split setupHotKeys into its own file

--- SupClient/src/index.ts
+++ SupClient/src/index.ts
@@ -1,9 +1,10 @@
 import * as io from "socket.io-client";
 
 import ProjectClient from "./ProjectClient"
+import setupHotKeys from "./setupHotKeys";
 import * as component from "./component";
 import * as dialogs from "./dialogs/index";
-export { ProjectClient, component, dialogs };
+export { ProjectClient, setupHotKeys, component, dialogs };
 
 let pluginsXHR = new XMLHttpRequest();
 pluginsXHR.open('GET', '/plugins.json', false); // Synchronous
@@ -89,51 +90,6 @@ export function onDisconnected() {
   div.appendChild(h1);
   div.appendChild(button);
   document.body.appendChild(div);
-}
-
-export function setupHotkeys() {
-  document.addEventListener('keydown', (event) => {
-    if (document.querySelector(".dialog") != null) return;
-
-    // window.location.origin isn't listed in lib.d.ts as of TypeScript 1.5
-    let origin: string = (<any>window.location).origin;
-
-    function sendMessage(action: string) {
-      if (window.parent != null) window.parent.postMessage({ type: "hotkey", content: action }, origin);
-      else window.postMessage({ type: "hotkey", content: action }, origin);
-    }
-
-    if (event.keyCode == 78 && (event.ctrlKey || event.metaKey)) { // CTRL-N
-      event.preventDefault();
-      if (event.shiftKey) sendMessage("newFolder");
-      else sendMessage("newAsset");
-    }
-
-    if ((event.keyCode === 79 || event.keyCode === 80) && (event.ctrlKey || event.metaKey)) { // CTRL-O or CTRL-P
-      event.preventDefault(); sendMessage("searchEntry");
-    }
-
-    if (event.keyCode === 87 && (event.ctrlKey || event.metaKey)) { // CTRL-W
-      event.preventDefault(); sendMessage("closeTab");
-    }
-
-    if (event.keyCode === 9 && event.ctrlKey) { // CTRL-TAB
-      event.preventDefault()
-      if (event.shiftKey) sendMessage("previousTab");
-      else sendMessage("nextTab");
-    }
-
-    if (event.keyCode === 116 || (event.keyCode === 80 && event.metaKey)) { // F5 || Cmd-P
-      event.preventDefault(); sendMessage("run");
-    }
-    if (event.keyCode === 117 || (event.keyCode === 80 && event.metaKey && event.shiftKey)) { // F6 or Cmd-Shift-P
-      event.preventDefault(); sendMessage("debug");
-    }
-
-    if (event.keyCode === 123 && (<any>window).nwDispatcher != null) { // F12
-      event.preventDefault(); sendMessage("devtools");
-    }
-  });
 }
 
 export function getTreeViewInsertionPoint(treeView: any) {
---
+++ SupClient/src/setupHotKeys.ts
@@ -0,0 +1,44 @@
+export default function() {
+  document.addEventListener('keydown', (event) => {
+    if (document.querySelector(".dialog") != null) return;
+
+    // window.location.origin isn't listed in lib.d.ts as of TypeScript 1.5
+    let origin: string = (<any>window.location).origin;
+
+    function sendMessage(action: string) {
+      if (window.parent != null) window.parent.postMessage({ type: "hotkey", content: action }, origin);
+      else window.postMessage({ type: "hotkey", content: action }, origin);
+    }
+
+    if (event.keyCode == 78 && (event.ctrlKey || event.metaKey)) { // CTRL-N
+      event.preventDefault();
+      if (event.shiftKey) sendMessage("newFolder");
+      else sendMessage("newAsset");
+    }
+
+    if ((event.keyCode === 79 || event.keyCode === 80) && (event.ctrlKey || event.metaKey)) { // CTRL-O or CTRL-P
+      event.preventDefault(); sendMessage("searchEntry");
+    }
+
+    if (event.keyCode === 87 && (event.ctrlKey || event.metaKey)) { // CTRL-W
+      event.preventDefault(); sendMessage("closeTab");
+    }
+
+    if (event.keyCode === 9 && event.ctrlKey) { // CTRL-TAB
+      event.preventDefault()
+      if (event.shiftKey) sendMessage("previousTab");
+      else sendMessage("nextTab");
+    }
+
+    if (event.keyCode === 116 || (event.keyCode === 80 && event.metaKey)) { // F5 || Cmd-P
+      event.preventDefault(); sendMessage("run");
+    }
+    if (event.keyCode === 117 || (event.keyCode === 80 && event.metaKey && event.shiftKey)) { // F6 or Cmd-Shift-P
+      event.preventDefault(); sendMessage("debug");
+    }
+
+    if (event.keyCode === 123 && (<any>window).nwDispatcher != null) { // F12
+      event.preventDefault(); sendMessage("devtools");
+    }
+  });
+}
