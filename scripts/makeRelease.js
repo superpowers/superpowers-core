00dede3533030e55bd4aff8fb2ae94728c772026
Elisée Maurer <elisee@sparklinlabs.com>
2015-06-19 20:11:51 +0200

    Fix makeRelease.js

--- scripts/makeRelease.js
+++ scripts/makeRelease.js
@@ -13,8 +13,8 @@ catch (error) { console.log("Could not create superpowers-" + packageInfo.versio
 
 function shouldDistribute(file) {
   file = file.substring(sourceRootPath.length + 1).replace(/\\/g, "/");
-  if (file[0] === ".") return false;
-  if (_.endsWith(file, "gulpfile.coffee")) return false;
+  if (file[0] === "." || file.indexOf("/.") !== -1) return false;
+  if (_.endsWith(file, "gulpfile.js") || _.endsWith(file, "tsconfig.json")) return false;
   if (_.endsWith(file, "launcher.cmd")) return false;
   if (_.endsWith(file, ".orig")) return false;
   if (_.endsWith(file, ".jade")) return false;
@@ -22,12 +22,11 @@ function shouldDistribute(file) {
   if (_.endsWith(file, ".ts") && file.indexOf("/typings/") === -1 && file.indexOf("/node_modules/") === -1) return false;
   if (_.startsWith(file, "client")) return false;
   if (_.startsWith(file, "scripts")) return false;
-  if (_.startsWith(file, "projects")) return false;
   if (_.startsWith(file, "node_modules/browserify")) return false;
-  if (_.startsWith(file, "node_modules/coffeeify")) return false;
+  if (_.startsWith(file, "node_modules/brfs")) return false;
+  if (_.startsWith(file, "node_modules/vinyl-source-stream")) return false;
   if (_.startsWith(file, "node_modules/watchify")) return false;
   if (_.startsWith(file, "node_modules/gulp")) return false;
-  if (_.startsWith(file, "node_modules/.bin")) return false;
   if (_.startsWith(file, "launcher/src")) return false;
   if (_.startsWith(file, "bin")) return false;
   if (_.startsWith(file, "builds") || _.startsWith(file, "projects")) return false;
