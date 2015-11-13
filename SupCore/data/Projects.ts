6afb1a20fb2a1f5d363d6f8151c58eb36f93a69c
Elisée Maurer <elisee@sparklinlabs.com>
2015-11-13 18:11:38 +0100

    SupCore: Workaround NW.js bug when sorting projects, leading to an infinite loop

--- SupCore/data/Projects.ts
+++ SupCore/data/Projects.ts
@@ -12,7 +12,15 @@ export default class Projects extends ListById {
   }
   
   static sort(a: SupCore.Data.ProjectManifestPub, b: SupCore.Data.ProjectManifestPub) {
-    return a.name.localeCompare(b.name);
+    // FIXME: This fails in NW.js, we can enable it back
+    // once the switch to Electron is done.
+    // return a.name.localeCompare(b.name);
+
+    let aName = a.name.toLowerCase();
+    let bName = b.name.toLowerCase();
+    if (aName < bName) return -1;
+    if (aName > bName) return 1;
+    return 0;
   }
 
   pub: SupCore.Data.ProjectManifestPub[];
