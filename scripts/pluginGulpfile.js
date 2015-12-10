8c5b5ea7666d957002a50c23c758dacf31c3f2d8
Bilou84 <nicolas.gauthier84@gmail.com>
2015-12-10 15:50:36 +0100

    Clean up plugin gulpfile to remove unnecessary tasks

--- scripts/pluginGulpfile.js
+++ scripts/pluginGulpfile.js
@@ -2,37 +2,42 @@ var gulp = require("gulp");
 var tasks = [];
 var fs = require("fs");
 
-// Jade
-var jade = require("gulp-jade");
-var rename = require("gulp-rename");
-var pluginI18n = require("./pluginI18n");
-var locales = [ "en" ];
-try { locales = fs.readdirSync(pluginI18n.rootLocalesPath); } catch (err) { /* Ignore */ }
+var editors = [];
+try { editors = fs.readdirSync("./editors"); } catch (err) { /* Ignore */ }
 
-locales.forEach(function(locale) {
-  var contexts = pluginI18n.loadLocale(locale);
-  gulp.task("jade-" + locale, function() {
-    var result = gulp.src("./editors/**/index.jade").pipe(jade({ locals: { t: pluginI18n.makeT(contexts) } }));
-    if (locale !== "en") result.pipe(rename({ extname: "." + locale + ".html" }));
-    return result.pipe(gulp.dest("./public/editors"));
+if (editors.length > 0) {
+  // Jade
+  var jade = require("gulp-jade");
+  var rename = require("gulp-rename");
+  var pluginI18n = require("./pluginI18n");
+  var locales = [ "en" ];
+  try { locales = fs.readdirSync(pluginI18n.rootLocalesPath); } catch (err) { /* Ignore */ }
+
+  locales.forEach(function(locale) {
+    var contexts = pluginI18n.loadLocale(locale);
+    gulp.task("jade-" + locale, function() {
+      var result = gulp.src("./editors/**/index.jade").pipe(jade({ locals: { t: pluginI18n.makeT(contexts) } }));
+      if (locale !== "en") result.pipe(rename({ extname: "." + locale + ".html" }));
+      return result.pipe(gulp.dest("./public/editors"));
+    });
+    tasks.push("jade-" + locale);
   });
-  tasks.push("jade-" + locale);
-});
 
-gulp.task("jade-none", function() {
-  return gulp.src("./editors/**/index.jade")
-    .pipe(jade({ locals: { t: function(path) { return path; } } }))
-    .pipe(rename({ extname: ".none.html" }))
-    .pipe(gulp.dest("./public/editors"));
-});
-tasks.push("jade-none");
+  gulp.task("jade-none", function() {
+    return gulp.src("./editors/**/index.jade")
+      .pipe(jade({ locals: { t: function(path) { return path; } } }))
+      .pipe(rename({ extname: ".none.html" }))
+      .pipe(gulp.dest("./public/editors"));
+  });
+  tasks.push("jade-none");
 
-// Stylus
-var stylus = require("gulp-stylus");
-gulp.task("stylus", function() {
-  return gulp.src("./editors/**/index.styl").pipe(stylus({ errors: true, compress: true })).pipe(gulp.dest("./public/editors"));
-});
-tasks.push("stylus");
+  // Stylus
+  var stylus = require("gulp-stylus");
+  gulp.task("stylus", function() {
+    return gulp.src("./editors/**/index.styl").pipe(stylus({ errors: true, compress: true })).pipe(gulp.dest("./public/editors"));
+  });
+  tasks.push("stylus");
+}
 
 // TypeScript
 var ts = require("gulp-typescript");
@@ -50,6 +55,7 @@ var vinylSourceStream = require("vinyl-source-stream");
 function makeBrowserify(source, destination, output) {
   gulp.task(output + "-browserify", [ "typescript" ], function() {
     if (!fs.existsSync(source)) return;
+
     var bundler = browserify(source);
     bundler.transform("brfs");
     function bundle() { return bundler.bundle().pipe(vinylSourceStream(output + ".js")).pipe(gulp.dest(destination)); };
@@ -65,9 +71,6 @@ makeBrowserify("./runtime/index.js", "./public", "runtime");
 makeBrowserify("./api/index.js", "./public", "api");
 makeBrowserify("./settingsEditors/index.js", "./public", "settingsEditors");
 makeBrowserify("./documentation/index.js", "./public", "documentation");
-
-var editors = [];
-try { editors = fs.readdirSync("./editors"); } catch (err) { /* Ignore */ }
 editors.forEach(function(editor) { makeBrowserify("./editors/" + editor + "/index.js", "./public/editors", editor + "/index"); });
 
 // All
