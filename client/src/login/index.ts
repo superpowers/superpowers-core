e87302160360158d16972372071d48d86caa00db
Pangoraw <naydex.mc@gmail.com>
2016-01-15 20:56:11 +0100

    client: Hide login form until `superpowers.json` has been loaded, fixes #34

--- client/src/login/index.ts
+++ client/src/login/index.ts
@@ -2,8 +2,12 @@ import "../window";
 
 const port = (window.location.port.length === 0) ? "80" : window.location.port;
 
+const connectingElt = document.querySelector(".connecting") as HTMLDivElement;
+const formElt = document.querySelector(".login") as HTMLDivElement;
 const serverPasswordElt = document.querySelector(".server-password") as HTMLInputElement;
 const usernameElt = document.querySelector(".username") as HTMLInputElement;
+
+formElt.hidden = true;
 
 let supServerAuth = SupClient.cookies.getJSON("supServerAuth");
 
@@ -26,6 +30,8 @@ SupClient.fetch("superpowers.json", "json", (err, data) => {
 });
 
 function start() {
+  formElt.hidden = false;
+  connectingElt.hidden = true;
   document.querySelector(".server-name").textContent = SupClient.i18n.t("hub:serverAddress", { hostname: window.location.hostname, port });
   document.querySelector("form.login").addEventListener("submit", onFormSubmit);
 }
