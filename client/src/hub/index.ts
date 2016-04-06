e5a776da3757204b3212bd4f681c2c7f304b57a8
Elisée Maurer <elisee@sparklinlabs.com>
2016-04-06 04:58:45 +0200

    client: Use window.open to open projects in the app

--- client/src/hub/index.ts
+++ client/src/hub/index.ts
@@ -212,10 +212,8 @@ function onProjectActivate() {
   const projectId = ui.projectsTreeView.selectedNodes[0].dataset["id"];
   const href = `/project/?project=${projectId}`;
 
-  if (SupClient.isApp) {
-    const url = `${window.location.origin}${href}`;
-    window.top.postMessage({ type: "new-standalone-window", url, title: data.projects.byId[projectId].name }, "file://");
-  } else window.location.href = href;
+  if (SupClient.isApp) window.open(`${window.location.origin}${href}`);
+  else window.location.href = href;
 }
 
 let autoOpenProject = true;
