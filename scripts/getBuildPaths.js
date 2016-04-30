41ee394f5b65f06bdc2aeadd2a6b60637b9f7508
bilou84 <nicolas.gauthier84@gmail.com>
2016-04-30 16:39:32 +0200

    scripts: Ignore systems and plugins with no git folder in build script

--- scripts/getBuildPaths.js
+++ scripts/getBuildPaths.js
@@ -2,6 +2,7 @@ var path = require("path");
 var fs = require("fs");
 
 function shouldIgnoreFolder(folderName) { return folderName.indexOf(".") !== -1 || folderName === "node_modules" || folderName === "public"; }
+var builtInFolderAuthors = [ "default", "common", "extra" ];
 
 module.exports = function(rootPath) {
   var buildPaths = [
@@ -18,23 +19,37 @@ module.exports = function(rootPath) {
   try { systemFolders = fs.readdirSync(systemsPath); } catch (err) { /* Ignore */ }
   systemFolders.forEach(function(systemName) {
     if (shouldIgnoreFolder(systemName)) return;
-  
+
     var systemPath = systemsPath + "/" + systemName;
+
+    var isDevFolder = true;
+    try { fs.readdirSync(systemPath + "/" + ".git"); } catch (err) { isDevFolder = false; }
+    if (!isDevFolder) return;
+
     fs.readdirSync(systemPath).forEach(function(systemFolder) {
       if (shouldIgnoreFolder(systemFolder) || systemFolder === "plugins") return;
       buildPaths.push(systemPath + "/" + systemFolder);
     });
-  
+
     var systemPluginsPath = systemPath + "/plugins";
     if (!fs.existsSync(systemPluginsPath)) return;
 
     fs.readdirSync(systemPluginsPath).forEach(function(pluginAuthor) {
       if (shouldIgnoreFolder(pluginAuthor)) return;
-  
+
       var pluginAuthorPath = systemPluginsPath + "/" + pluginAuthor;
       fs.readdirSync(pluginAuthorPath).forEach(function(pluginName) {
         if (shouldIgnoreFolder(pluginName)) return;
-        buildPaths.push(pluginAuthorPath + "/" + pluginName);
+        
+        var pluginPath = pluginAuthorPath + "/" + pluginName;
+
+        if (builtInFolderAuthors.indexOf(pluginAuthor) === -1) {
+          var isDevFolder = true;
+          try { fs.readdirSync(pluginPath + "/" + ".git"); } catch (err) { isDevFolder = false; }
+          if (!isDevFolder) return;
+        }
+
+        buildPaths.push(pluginPath);
       });
     });
   });
