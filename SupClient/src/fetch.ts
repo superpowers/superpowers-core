fd21e12e09d3fb1a1d277084d6cd8ff2298276dd
vianney <vpetit@3dduo.com>
2016-10-26 19:21:10 +0200

    Supclient: fetch: workaround for ie11.

--- SupClient/src/fetch.ts
+++ SupClient/src/fetch.ts
@@ -9,7 +9,18 @@ export default function fetch(url: string, type: string, callback: (err: Error, 
       return;
     }
 
-    callback(null, xhr.response);
+    // Workaround for IE11
+    let response = xhr.response;
+    if (type === "json" && typeof xhr.response === "string") {
+      console.log("SupClient: fetch: response was expected to be json, got a string instead. Now attempting to parse it as json...");
+      try {
+        response = JSON.parse(xhr.response);
+      } catch (e) {
+        console.log(e);
+        console.log("SupClient: fetch: failed to parse response as json.");
+      }
+    }
+    callback(null, response);
   };
 
   xhr.onerror = (event) => {
