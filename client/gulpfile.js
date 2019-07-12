b5e3b2edd8338179cf340ade305923ff61f9f056
bilou84 <nicolas.gauthier84@gmail.com>
2019-07-12 13:52:08 +0200

    Fix typescript errors not failing a gulp build correctly (it broke when we updated to gulp 4)

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -33,14 +33,11 @@ const tsProject = ts.createProject("./tsconfig.json");
 const tslint = require("gulp-tslint");
 
 gulp.task("typescript", () => {
-  let failed = false;
   const tsResult = tsProject.src()
     .pipe(tslint({ formatter: "prose" }))
     .pipe(tslint.report({ emitError: true }))
     .on("error", (err) => { throw err; })
     .pipe(tsProject())
-    .on("error", () => { failed = true; })
-    .on("end", () => { if (failed) throw new Error("There were TypeScript errors."); });
   return tsResult.js.pipe(gulp.dest("./"));
 });
 
